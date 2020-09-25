57cb64e524bdf022d760f7f07f63b5f8ae456033
CorneliuSandu <sandu.corneliu@isa.utm.md>
2020-09-25 16:39:33 +0300

    proiect

---
+++ src/Telephone.java
@@ -0,0 +1,22 @@
+public class Telephone {
+
+private String marca;
+private String culoarea;
+private int anul;
+private String android;
+
+
+
+public Telephone(String marca, String culoarea, int anul, String android) {
+
+    this.marca = marca;
+    this.culoarea = culoarea;
+    this.anul = anul;
+    this.android = android;
+
+}
+
+
+
+
+}
